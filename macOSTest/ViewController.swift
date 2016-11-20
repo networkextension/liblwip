bb195124f72f7431a967e2b95f672afa8cb65015
yarshure <support@abigt.net>
2016-11-20 11:27:54 +0800

    add macOS Test

---
+++ macOSTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macOSTest
+//
+//  Created by yarshure on 2016/11/20.
+//  Copyright © 2016年 Kong XiangBo. All rights reserved.
+//
+
+import Cocoa
+import lwip
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
